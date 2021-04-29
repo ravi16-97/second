3e7dfdbcc4b4f39c4fe3ce5a95e4fb601beb0c84
Ravi Prasad <raviprasadc16@gmail.com>
2021-04-29 16:55:01 +0530

    First commit

---
+++ src/test/java/testpack/chrome.java
@@ -0,0 +1,30 @@
+package testpack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class chrome {
+	public static WebDriver driver;
+  @Test
+  
+  public void browse() throws Throwable {
+	  WebDriverManager.chromedriver().setup(); 
+	 driver = new ChromeDriver();
+	 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	 
+	driver.get("https://www.youtube.com/watch?v=c86GdHQaLsY");
+	Thread.sleep(5000);
+	
+  }
+@AfterTest
+public void teardown() {
+	driver.quit();
+}
+
+}
